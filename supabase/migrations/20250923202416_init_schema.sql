1b4cd284e619fe5708bb471aed4e217d7a95e5ef
Neil <morrison.neil.v@gmail.com>
2025-09-23 13:26:14 -0700

    init_schema

---
+++ supabase/migrations/20250923202416_init_schema.sql
@@ -0,0 +1,87 @@
+create table "public"."user_profiles" (
+    "id" uuid not null default gen_random_uuid(),
+    "created_at" timestamp without time zone default now(),
+    "updated_at" timestamp without time zone default now(),
+    "deleted_at" timestamp without time zone,
+    "first_name" text,
+    "last_name" text,
+    "middle_name" text,
+    "email" text not null,
+    "auth_user_id" uuid
+);
+
+CREATE INDEX idx_user_profiles_auth_user_id ON public.user_profiles USING btree (auth_user_id);
+
+CREATE INDEX idx_user_profiles_email ON public.user_profiles USING btree (email);
+
+CREATE UNIQUE INDEX user_profiles_email_key ON public.user_profiles USING btree (email);
+
+CREATE UNIQUE INDEX user_profiles_pkey ON public.user_profiles USING btree (id);
+
+alter table "public"."user_profiles" add constraint "user_profiles_pkey" PRIMARY KEY using index "user_profiles_pkey";
+
+alter table "public"."user_profiles" add constraint "user_profiles_auth_user_id_fkey" FOREIGN KEY (auth_user_id) REFERENCES auth.users(id) not valid;
+
+alter table "public"."user_profiles" validate constraint "user_profiles_auth_user_id_fkey";
+
+alter table "public"."user_profiles" add constraint "user_profiles_email_key" UNIQUE using index "user_profiles_email_key";
+
+set check_function_bodies = off;
+
+CREATE OR REPLACE FUNCTION public.handle_new_user()
+ RETURNS trigger
+ LANGUAGE plpgsql
+ SECURITY DEFINER
+AS $function$begin
+  insert into public.user_profiles (auth_user_id, first_name, last_name, email)
+  values (
+    new.id,
+    new.raw_user_meta_data->>'first_name',
+    new.raw_user_meta_data->>'last_name',
+    new.email
+  );
+  return new;
+end;$function$
+;
+
+grant delete on table "public"."user_profiles" to "anon";
+
+grant insert on table "public"."user_profiles" to "anon";
+
+grant references on table "public"."user_profiles" to "anon";
+
+grant select on table "public"."user_profiles" to "anon";
+
+grant trigger on table "public"."user_profiles" to "anon";
+
+grant truncate on table "public"."user_profiles" to "anon";
+
+grant update on table "public"."user_profiles" to "anon";
+
+grant delete on table "public"."user_profiles" to "authenticated";
+
+grant insert on table "public"."user_profiles" to "authenticated";
+
+grant references on table "public"."user_profiles" to "authenticated";
+
+grant select on table "public"."user_profiles" to "authenticated";
+
+grant trigger on table "public"."user_profiles" to "authenticated";
+
+grant truncate on table "public"."user_profiles" to "authenticated";
+
+grant update on table "public"."user_profiles" to "authenticated";
+
+grant delete on table "public"."user_profiles" to "service_role";
+
+grant insert on table "public"."user_profiles" to "service_role";
+
+grant references on table "public"."user_profiles" to "service_role";
+
+grant select on table "public"."user_profiles" to "service_role";
+
+grant trigger on table "public"."user_profiles" to "service_role";
+
+grant truncate on table "public"."user_profiles" to "service_role";
+
+grant update on table "public"."user_profiles" to "service_role";
